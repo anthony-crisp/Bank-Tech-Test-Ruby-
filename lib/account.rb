2816feba41d80961e4c6fc1b30613883c8bb6d2e
Anthony Crisp <anthony_crisp@hotmail.com>
2017-06-12 14:06:16 +0100

    transactions are stored with date

--- lib/account.rb
+++ lib/account.rb
@@ -1,17 +1,26 @@
+require 'date'
+
 class Account
 
-  attr_reader :balance
+  attr_reader :balance, :transactions, :date
 
   def initialize
     @balance = 0
+    @transactions = ["date || credit || debit || balance"]
+  end
+
+  def todays_date
+    Date.today.strftime('%d/%m/%Y')
   end
 
   def deposit(ammount)
     @balance += ammount
+    @transactions << "#{todays_date} || #{'%.2f' % ammount} || || #{'%.2f' % balance}"
   end
 
   def withdraw(ammount)
     @balance -= ammount
+    @transactions << "#{todays_date} || || #{'%.2f' % ammount} || #{'%.2f' % balance}"
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,16 +8,40 @@ RSpec.describe Account do
     expect(subject.balance).to eq(0)
   end
 
+  it 'initializes with with no transactions' do
+    expect(subject.transactions).to eq ["date || credit || debit || balance"]
+  end
+
+  it 'stores transactions' do
+    subject.deposit(100)
+    subject.withdraw(10)
+    subject.withdraw(50)
+    expect(subject.transactions.count).to eq 4
+  end
+
   describe '#deposit' do
     it 'can recieve a deposit of 10' do
       expect { subject.deposit(10) }.to change { subject.balance }.by 10
+    end
+
+    it 'can recieve multiple deposits' do
+      subject.deposit(10)
+      subject.deposit(30)
+      expect(subject.balance).to eq 40
     end
   end
 
   describe '#withdraw' do
     it 'can be debited by 10' do
       subject.deposit(100)
-      expect { subject.withdraw(10) }.to change { subject.balance}.by(-10)
+      expect { subject.withdraw(10) }.to change { subject.balance }.by(-10)
+    end
+
+    it 'can be debited multiple times' do
+      subject.deposit(100)
+      subject.withdraw(10)
+      subject.withdraw(50)
+      expect(subject.balance).to eq 40
     end
   end
 
