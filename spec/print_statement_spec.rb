833e1aac0b7ab569f8e4892e28ef028c695d1e77
Anthony Crisp <anthony_crisp@hotmail.com>
2017-06-12 16:48:51 +0100

    formatting

--- spec/print_statement_spec.rb
+++ spec/print_statement_spec.rb
@@ -2,8 +2,8 @@ require 'account'
 require 'print_statement'
 
 RSpec.describe PrintStatement do
-    subject(:print_statement) { described_class.new(["date || credit || debit || balance"]) }
-    it 'outputs to stdout' do
-      expect { subject.print_statement }.to output("date || credit || debit || balance\n").to_stdout
-    end
+  subject(:print_statement) { described_class.new(["date || credit || debit || balance"]) }
+  it 'outputs to stdout' do
+    expect { subject.print_statement }.to output("date || credit || debit || balance\n").to_stdout
+  end
 end
